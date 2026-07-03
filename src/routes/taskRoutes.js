cdb28f17e16a8d2a573ad3e29847c7aa862bc271
Dharmik Rabadiya <djrabadiya2910@gmail.com>
2026-07-03 15:05:49 +0530

    Initial commit

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,14 +1,3 @@
-// const express = require('express');
-
-// const taskController = require('../controllers/taskController');
-// const router = express.Router();
-
-// router.post('/', taskController.createTask);
-// router.get('/', taskController.getTasks);
-// router.put('/:id', taskController.updateTask);
-// router.delete('/:id', taskController.deleteTask);
-
-// module.exports = router;
 const express = require('express');
 const taskController = require('../controllers/taskController');
 
