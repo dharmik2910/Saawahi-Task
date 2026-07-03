5c21e24b657cd5b7b355a2c6dd92b0ef4f71af44
Dharmik Rabadiya <djrabadiya2910@gmail.com>
2026-07-03 15:04:58 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,25 +1,3 @@
-// const express = require('express');
-
-// const authRouter = require('./routes/authRoutes');
-// const taskRouter = require('./routes/taskRoutes');
-// const { notFound, errorHandler } = require('./middlewares/errorHandler');
-
-// const app = express();
-
-// app.use(express.json());
-
-// app.get('/health', (req, res) => {
-//     res.status(200).json({ message: 'Server is healthy' });
-// });
-
-// app.use('/api/v1/auth', authRouter);
-// app.use('/api/v1/tasks', taskRouter);
-
-// app.use(notFound);
-// app.use(errorHandler);
-
-// module.exports = app;
-
 const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
